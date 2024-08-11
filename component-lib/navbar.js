445ed2e34fe55ebcce56bca87420ddc5acfe8453
iamanojkumar <manojgenius7@gmail.com>
2024-08-11 17:10:15 +0530

    All basic setup

--- component-lib/navbar.js
+++ component-lib/navbar.js
@@ -2,7 +2,7 @@ class Navbar extends HTMLElement {
     connectedCallback() {
         const link = document.createElement('link');
         link.rel = 'stylesheet';
-        link.href = 'style-sheet/navbar.css';
+        link.href = '/style-sheet/navbar.css';
         document.head.appendChild(link);
         this.innerHTML = `
     <div class="nav-bar">
@@ -17,27 +17,3 @@ class Navbar extends HTMLElement {
 }
 
 customElements.define('nav-bar', Navbar);
-
-
-
-// class Navbar extends HTMLElement {
-//     connectedCallback() {
-//         const link = document.createElement('link');
-//         link.rel = 'stylesheet';
-//         link.href = 'style-sheet/navbar.css';
-//         document.head.appendChild(link);
-//         this.innerHTML = `
-
-        
-//             <div class="nav-bar">
-//                 <div class="brand-logo">Kuartist</div>
-//                     <div class="nav-lnk-lst-wrp">
-//                     <a class="nav-link-txt" href="pages/about.html">About Us</a>
-//                     <a class="nav-link-txt" href="pages/contact-us.html">Contact Us</a>
-//                     </div>
-//             </div>
-//         `;
-//     }
-// }
-
-// customElements.define('nav-bar', Navbar);

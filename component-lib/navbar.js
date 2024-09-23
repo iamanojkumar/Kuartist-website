504f4a498a8e288f04dba8b29f73c3ea3b52e705
iamanojkumar <manojgenius7@gmail.com>
2024-09-23 17:52:44 +0530

    mobile optimization and minor desktop changes

--- component-lib/navbar.js
+++ component-lib/navbar.js
@@ -5,15 +5,22 @@ class Navbar extends HTMLElement {
         link.href = '/style-sheet/navbar.css';
         document.head.appendChild(link);
         this.innerHTML = `
-    <div class="nav-bar">
-        <div class="brand-logo">Kũartist</div>
-        <div class="nav-lnk-lst-wrp">
-            <a class="nav-link-txt" href="/">Home</a>
-            <a class="nav-link-txt" href="/pages/projects.html">Projects</a>
-            <button class="btn-cta" type="button" onclick="window.open('https://forms.zoho.in/kuartist/form/Contact');">Contact Us</button>
-        </div>
-    </div>
-`;
+            <div class="nav-bar">
+                <div class="brand-logo">Kũartist</div>
+                <div class="hamburger" id="hamburger">&#9776;</div>
+                <div class="nav-lnk-lst-wrp" id="nav-links">
+                    <a class="nav-link-txt" href="/">Home</a>
+                    <a class="nav-link-txt" href="/pages/projects.html">Projects</a>
+                    <button class="btn-cta" type="button" onclick="window.open('https://forms.zoho.in/kuartist/form/Contact');">Contact Us</button>
+                </div>
+            </div>
+        `;
+
+        // Add event listener for the hamburger menu
+        this.querySelector('#hamburger').addEventListener('click', () => {
+            const navLinks = this.querySelector('#nav-links');
+            navLinks.classList.toggle('active'); // Toggle 'active' class
+        });
     }
 }
 

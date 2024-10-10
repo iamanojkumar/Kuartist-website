95386e859912eedf9add2322950aff5818445516
iamanojkumar <manojgenius7@gmail.com>
2024-10-11 02:05:56 +0530

    Optimized preloader and SEO for project template pages, added webclip image

--- js/lottie-animation.js
+++ js/lottie-animation.js
@@ -4,15 +4,13 @@ window.addEventListener('load', function() {
         container: document.getElementById('lottie-animation'), // ID of the container
         path: 'Assets/loading animation/preloader-animation.json', // Path to your JSON animation file
         renderer: 'svg',
-        loop: true,
+        loop: true, // Loop indefinitely
         autoplay: true,
     });
 
     // Hide preloader after the page is fully loaded
     const preloader = document.getElementById('preloader');
     
-    // Use a timeout to ensure the animation plays for a moment before hiding
-    setTimeout(() => {
-        preloader.style.display = 'none'; // Hide preloader after loading
-    }, 500); // Adjust time as needed (in milliseconds)
+    // Immediately hide the preloader once the page is loaded
+    preloader.style.display = 'none'; // Hide preloader after loading
 });
